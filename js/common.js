cdc20dbf981a83af407a1b2a23776668ea7ac44f
Xu Jing <734332894@qq.com>
2018-06-20 09:41:35 +0800

    登录页面提交

--- js/common.js
+++ js/common.js
@@ -9,3 +9,18 @@ $(function(){
 	});
 
 });
+
+
+var APP = {
+	baseUrl:'http://fullstack.net.cn:3000'
+}
+//将表单数据以Json的形式返回
+$.fn.serializeToJson = function(){
+	var formArray = this.serializeArray();
+	var result = {};
+	formArray.forEach(function(item,index){
+	  result[item.name] = item.value;
+	
+	})
+	return result;
+  }
---
+++ js/login.js
@@ -0,0 +1,32 @@
+$(function(){
+ //1.为登录按钮添加点击事件
+ //2.获取用户输入的用户名和密码
+ //3.对用户输入的信息格式进行校验
+ //4.如果校验成功，调用登录接口，实现登录
+ $('#loginBtn').on('click',function(){
+    var result = $('#loginForm').serializeToJson();
+    if(!$.trim(result.username)){
+        alert('请输入用户名');
+        return;
+    }
+    if(!$.trim(result.password)){
+        alert('请输入密码');
+        return;
+    }
+
+    $.ajax({
+        type:'post',
+        url:`${APP.baseUrl}/employee/employeeLogin`,
+        data:result,
+        success:function(response){
+            if(response.success){
+                alert('登录成功')
+                location.href = "user.html"
+            } else {
+                alert(response.message)
+            }
+        }
+    })
+})
+
+})
